202af62d0410d5a0a41d78f859fbbafb104bd05d
eric183 <kk297466058@gmail.com>
2023-05-26 14:02:23 +0800

    updated backend

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ConfigModule } from '@nestjs/config';
+ConfigModule.forRoot({
+  isGlobal: true,
+});
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(80);
+  app.enableCors();
+
+  await app.listen(process.env.PORT, '0.0.0.0', () => {
+    console.log('Server Run Port', process.env.PORT);
+  });
 }
 bootstrap();
